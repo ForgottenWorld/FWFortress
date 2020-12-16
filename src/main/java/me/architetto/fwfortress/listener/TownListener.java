9623661d9b3cb63c9525bafe09d569e902ee0c07
Vincenzo D'Agostino <vind.2nd@gmail.com>
2020-12-16 09:08:20 +0100

    + onTownDelete event fixed ! maybe

--- src/main/java/me/architetto/fwfortress/listener/TownListener.java
+++ src/main/java/me/architetto/fwfortress/listener/TownListener.java
@@ -1,6 +1,9 @@
 package me.architetto.fwfortress.listener;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.event.DeleteTownEvent;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import me.architetto.fwfortress.fortress.Fortress;
 import me.architetto.fwfortress.fortress.FortressService;
 import me.architetto.fwfortress.util.ChatFormatter;
@@ -20,22 +23,32 @@ public class TownListener implements Listener {
         FortressService fortressService = FortressService.getInstance();
         List<Fortress> fortresses = new ArrayList<>(fortressService.getFortressContainer().values());
 
-
         fortresses.forEach(fortress -> {
-            if (fortress.getCurrentOwner().equals(event.getTownName())) {
-                if (!fortress.getFirstOwner().equals(event.getTownName())) {
-                    Bukkit.broadcastMessage(ChatFormatter.formatMessage(ChatColor.AQUA + "La fortezza " + ChatColor.YELLOW
-                            + fortress.getFortressName() + ChatColor.AQUA + " e' tornata sotto il controllo di " +
-                            ChatColor.YELLOW + fortress.getFirstOwner()));
+            if (fortress.getFirstOwner().equals(event.getTownName())) {
 
-                    fortressService.getFortressContainer().get(fortress.getFortressName())
-                            .setCurrentOwner(fortress.getFirstOwner());
-                    fortressService.updateFortressFile(fortress.getFortressName());
-                } else {
-                    Bukkit.broadcastMessage(ChatFormatter.formatMessage(ChatColor.AQUA + "La fortezza " + ChatColor.YELLOW
-                            + fortress.getFortressName() + ChatColor.AQUA + " e' caduta in rovina"));
-                    fortressService.removeFortress(fortress.getFortressName());
+                Bukkit.broadcastMessage(ChatFormatter.formatMessage(ChatColor.AQUA + "La fortezza " + ChatColor.YELLOW
+                        + fortress.getFortressName() + ChatColor.AQUA + " e' caduta in rovina"));
+                if (!fortress.getFirstOwner().equals(fortress.getCurrentOwner())) {
+
+                    try {
+                        TownyMessaging.sendTownMessagePrefixed(TownyAPI.getInstance().getDataSource().getTown(fortress.getCurrentOwner()),
+                                ChatColor.AQUA + "La citta' ha perso il controllo di una fortezza : "
+                                        + ChatColor.YELLOW + fortress.getFortressName());
+                    } catch (NotRegisteredException e) {
+                        e.printStackTrace();
+                    }
                 }
+
+                fortressService.removeFortress(fortress.getFortressName());
+
+            } else if (fortress.getCurrentOwner().equals(event.getTownName())) {
+                Bukkit.broadcastMessage(ChatFormatter.formatMessage(ChatColor.AQUA + "La fortezza " + ChatColor.YELLOW
+                        + fortress.getFortressName() + ChatColor.AQUA + " e' tornata sotto il controllo di " +
+                        ChatColor.YELLOW + fortress.getFirstOwner()));
+
+                fortressService.getFortressContainer().get(fortress.getFortressName())
+                        .setCurrentOwner(fortress.getFirstOwner());
+                fortressService.updateFortressFile(fortress.getFortressName());
             }
         });
     }
